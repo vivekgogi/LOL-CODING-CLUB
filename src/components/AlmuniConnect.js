19de9e9e123c6b27937bd5f3bf0cdaf3d1a566c2
chinmay.annadate <ccannadate@gmail.com>
2023-10-27 20:40:03 +0530

    alumni database + useEffect fix in certverification

--- src/components/AlmuniConnect.js
+++ src/components/AlmuniConnect.js
@@ -1,14 +1,49 @@
-import { data23 } from "../data/alumni23";
-import { data22 } from "../data/alumni22";
-import { data21 } from "../data/alumni21";
+import { dbRef } from "../utils/firebase";
+import { child, get } from "firebase/database";
+import { useState, useEffect } from "react";
 import AlumniTable from "./AlumniTable";
 
 function AlumniConnect() {
+  const [alumni21, setAlumni21] = useState([]);
+  const [alumni22, setAlumni22] = useState([]);
+  const [alumni23, setAlumni23] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        var snapshot = await get(child(dbRef, "alumni21"));
+        if (snapshot.exists()) {
+          setAlumni21(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+
+        snapshot = await get(child(dbRef, "alumni22"));
+        if (snapshot.exists()) {
+          setAlumni22(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+        
+        snapshot = await get(child(dbRef, "alumni23"));
+        if (snapshot.exists()) {
+          setAlumni23(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="alumni-connect">
-      <AlumniTable key={2023} year={2023} data={data23} />
-      <AlumniTable key={2022} year={2022} data={data22} />
-      <AlumniTable key={2021} year={2021} data={data21} />
+      <AlumniTable key={2023} year={2023} data={alumni21} />
+      <AlumniTable key={2022} year={2022} data={alumni22} />
+      <AlumniTable key={2021} year={2021} data={alumni23} />
     </div>
   );
 }
--- src/pages/CertificateVerificationPage.js
+++ src/pages/CertificateVerificationPage.js
@@ -4,33 +4,35 @@ import Footer from "../components/Footer";
 import ResponsiveIframeContainer from "../components/ResponsiveIframeContainer";
 import { dbRef } from "../utils/firebase";
 import { child, get } from "firebase/database";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function CertificateVerificationPage() {
   const queryParameters = new URLSearchParams(window.location.search);
   const code = queryParameters.get("code");
 
-  const [driveID, setDriveID] = useState('');
+  const [driveID, setDriveID] = useState("");
 
-  get(child(dbRef, `certificates/${code}`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        setDriveID(snapshot.val());
-      } else {
-        console.log("No data available");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const snapshot = await get(child(dbRef, `certificates/${code}`));
+        if (snapshot.exists()) {
+          setDriveID(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      } catch (error) {
+        console.error(error);
       }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    };
+
+    fetchData();
+  }, [code]);
 
   return (
     <>
       <Header />
-      <ResponsiveIframeContainer
-        title="Certificate"
-        driveID={driveID}
-      />
+      <ResponsiveIframeContainer title="Certificate" driveID={driveID} />
       <Footer />
     </>
   );
